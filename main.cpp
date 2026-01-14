4744949581bcd75b3303e2116c78d4a5072a2a30
theJuniorProgrammer3 <175095475+theJuniorProgrammer3@users.noreply.github.com>
2026-01-14 18:02:50 +0800

    prototipe #3 (add multitouch support, end mouse support)

--- main.cpp
+++ main.cpp
@@ -70,40 +70,93 @@ int main() {
     int cX = 0;
     int cY = 0;
     bool down = false;
+    bool down2 = false;
+    SDL_FingerID fingerId1 = -1;
+SDL_FingerID fingerId2 = -1;
+    
         while (running) {
-                int mX, mY;
-                Uint32 mouseState = SDL_GetMouseState(&mX, &mY);
+                int mX, mY, mmX, mmY;
+                //SDL_FingerID fingerId1 = -1;
+//SDL_FingerID fingerId2 = -1;
+                //Uint32 mouseState = SDL_GetMouseState(&mX, &mY);
         while (SDL_PollEvent(&event)) {
 
 
             if (event.type == SDL_QUIT) running = false;
-            else if(event.type == SDL_MOUSEBUTTONDOWN) {
-               // mX = event.button.x;
-//                mY = event.button.y;
-                down = true;
-            } else if(event.type == SDL_MOUSEBUTTONUP) {
-    //            mX = event.button.x;
-//                mY = event.button.y;
-                down = false;
-            }
+
+// Finger down
+if (event.type == SDL_FINGERDOWN) {
+    if (!down) {
+        mX = event.tfinger.x * w;
+        mY = event.tfinger.y * h;
+        fingerId1 = event.tfinger.fingerId;
+        down = true;
+    } else {
+        mmX = event.tfinger.x * w;
+        mmY = event.tfinger.y * h;
+        fingerId2 = event.tfinger.fingerId;
+        down2 = true;
+    }
+}
+
+// Finger up
+if (event.type == SDL_FINGERUP) {
+    if (event.tfinger.fingerId == fingerId1) {
+        mX = -5;
+        mY = -5;
+        down = false;
+    } else if (event.tfinger.fingerId == fingerId2) {
+        mmX = -5;
+        mmY = -5;
+        down2 = false;
+    }
+}
 
             }
-                        if(down) {
-            if(mX >= buttonLeft.x && mX <= buttonLeft.x + buttonLeft.w && mY >= buttonLeft.y && mY  <= buttonLeft.y + buttonLeft.h ) {
-                    cX -= 10;
-                } else if(mX >= buttonRight.x && mX <= buttonRight.x + buttonRight.w && mY >= buttonRight.y && mY  <= buttonRight.y + buttonRight.h ) {
-                    cX += 10;
-                } else if(mX >= buttonUp.x && mX <= buttonUp.x + buttonUp.w && mY >= buttonUp.y && mY  <= buttonUp.y + buttonUp.h ) {
-                    cY -= 10;
-                } else if(mX >= buttonDown.x && mX <= buttonDown.x + buttonDown.w && mY >= buttonDown.y && mY  <= buttonDown.y + buttonDown.h ) {
-                    cY += 10;
-                } else if(mX >= buttonClick.x && mX <= buttonClick.x + buttonClick.w && mY >= buttonClick.y && mY  <= buttonClick.y + buttonClick.h ) {
-                    if(cX >= pX && cX <= pX + 128 && cY >= pY && cY <= pY + 128) {
-                        pX = rand() % (w - 128);
-                        pY = rand() % (h - 128);
-                    }
-                }
-        }
+                        if(down || down2) {
+                            // Button Left
+if (((mX >= buttonLeft.x && mX <= buttonLeft.x + buttonLeft.w) &&
+     (mY >= buttonLeft.y && mY <= buttonLeft.y + buttonLeft.h)) ||
+    ((mmX >= buttonLeft.x && mmX <= buttonLeft.x + buttonLeft.w) &&
+     (mmY >= buttonLeft.y && mmY <= buttonLeft.y + buttonLeft.h))) {
+    cX -= 10;
+}
+// Button Right
+if (((mX >= buttonRight.x && mX <= buttonRight.x + buttonRight.w) &&
+          (mY >= buttonRight.y && mY <= buttonRight.y + buttonRight.h)) ||
+         ((mmX >= buttonRight.x && mmX <= buttonRight.x + buttonRight.w) &&
+          (mmY >= buttonRight.y && mmY <= buttonRight.y + buttonRight.h))) {
+    cX += 10;
+}
+                        
+// Button Up
+if (((mX >= buttonUp.x && mX <= buttonUp.x + buttonUp.w) &&
+     (mY >= buttonUp.y && mY <= buttonUp.y + buttonUp.h)) ||
+    ((mmX >= buttonUp.x && mmX <= buttonUp.x + buttonUp.w) &&
+     (mmY >= buttonUp.y && mmY <= buttonUp.y + buttonUp.h))) {
+    cY -= 10;
+}
+// Button Down
+if (((mX >= buttonDown.x && mX <= buttonDown.x + buttonDown.w) &&
+          (mY >= buttonDown.y && mY <= buttonDown.y + buttonDown.h)) ||
+         ((mmX >= buttonDown.x && mmX <= buttonDown.x + buttonDown.w) &&
+          (mmY >= buttonDown.y && mmY <= buttonDown.y + buttonDown.h))) {
+    cY += 10;
+}
+// Button Click
+if (((mX >= buttonClick.x && mX <= buttonClick.x + buttonClick.w) &&
+          (mY >= buttonClick.y && mY <= buttonClick.y + buttonClick.h)) ||
+         ((mmX >= buttonClick.x && mmX <= buttonClick.x + buttonClick.w) &&
+          (mmY >= buttonClick.y && mmY <= buttonClick.y + buttonClick.h))) {
+    if (cX >= pX && cX <= pX + 128 && cY >= pY && cY <= pY + 128) {
+        pX = rand() % (w - 128);
+        pY = rand() % (h - 128);
+    }
+}
+                        }
+                            
+
+        
         SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
